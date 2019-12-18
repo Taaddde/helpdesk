47a6a175dd2a8d618fddb541fde1123179e4fc76
Tadeo <taaddde@hotmail.com>
2019-12-18 09:01:17 -0300

    B-1.70

--- client-test/src/app/app.component.ts
+++ client-test/src/app/app.component.ts
@@ -165,12 +165,7 @@ export class AppComponent implements OnInit {
               }
             },
             error =>{
-              var errorMessage = <any>error;
-              if(errorMessage != null){
-                var body = JSON.parse(error._body);
-                this.alertMessage = body.message;
-                console.error(error);
-              }
+              this.alertMessage = "Usuario/contraseña inválido";
             }
           )          
         }   
