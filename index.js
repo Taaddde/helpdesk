fa20efca55785a93a1274a4d1534a45860862fa8
Tadeo Rosochacki <taaddde@hotmail.com>
2020-11-13 10:13:21 -0300

    Colores

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var logger = require('./services/logger')
 var port = process.env.PORT || 3977;
+var colors = require('colors');
 
 mongoose.connect('mongodb://localhost:27017/helpdesk',{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, (err, res) =>{
     if(err){
         throw err;
     }else{
-        console.log('Aplicación TICKETS corriendo correctamente en puerto ' + port);
         app.listen(port, function(){
+            console.log(colors.cyan(' -----------------------------------------------------'))
+            console.log(`${colors.cyan('|')}                   ${colors.bgGreen('API INICIADA')}                      ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Aplicación: ${colors.green('Gestor de tickets HSJD')}                ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Entorno: ${colors.green('PROD')}                                     ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Puerto: ${colors.green(port)}                                      ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   DB: ${colors.green('localhost:27017/helpdesk')}                      ${colors.cyan('|')}`);
+            console.log(colors.cyan(' -----------------------------------------------------'))
             logger.info({message:{module:'index', msg:'Servidor iniciado en puerto '+port}});
         })
     }
--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');
 var app = require('./app');
 var logger = require('./services/logger')
 var port = process.env.PORT || 3978;
+var colors = require('colors');
 
 mongoose.connect('mongodb://localhost:27017/helpdesk-test',{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, (err, res) =>{
     if(err){
@@ -11,6 +12,13 @@ mongoose.connect('mongodb://localhost:27017/helpdesk-test',{ useUnifiedTopology:
     }else{
         console.log('Test esta corriendo correctamente');
         app.listen(port, function(){
+            console.log(colors.cyan(' -----------------------------------------------------'))
+            console.log(`${colors.cyan('|')}                   ${colors.bgGreen('API INICIADA')}                      ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Aplicación: ${colors.green('Gestor de tickets HSJD')}                ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Entorno: ${colors.green('TEST')}                                     ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   Puerto: ${colors.green(port)}                                      ${colors.cyan('|')}`);
+            console.log(`${colors.cyan('|')}   DB: ${colors.green('localhost:27017/helpdesk-test')}                 ${colors.cyan('|')}`);
+            console.log(colors.cyan(' -----------------------------------------------------'))
             logger.info({message:{module:'test', msg:'Servidor iniciado en puerto '+port}});
         })
     }
